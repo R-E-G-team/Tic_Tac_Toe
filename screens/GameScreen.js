127c5fd12e78494ca7e9b656a6b6ab4911398f2f
DongheonKim <oueya1479@naver.com>
2021-07-29 22:27:51 +0900

    Update GameScreen.js

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
+import playerCode from "../constants/player-code";
+
 const GameScreen = (props) => {
   const [titleMessage, setTitleMessage] = useState("Start Game!");
-  const [content, setContent] = useState("");
   const [playGround] = useState(
     Array.from({ length: 3 }, () => Array.from({ length: 3 }, () => 0))
   );
@@ -47,25 +48,70 @@ const GameScreen = (props) => {
     }
   };
 
-  const changeContent = (key) => {
-    setContent((currentContent) => {
-      if (currentContent === "") setContent("X");
-      else setContent("");
-    });
+  const getGroundData = (c, r) => {
+    if (c < 0) c = 3 + c;
+    if (r < 0) r = 3 + r;
+    return playGround[c][r];
   };
 
-  let squares = [];
-  for (let i = 0; i < 9; i++) {
-    squares.push(
-      <TouchableOpacity
-        key={i}
-        style={styles.buttonContainer}
-        onPress={changeContent.bind(this, i)}
-      >
-        <Text style={styles.content}>{content}</Text>
-      </TouchableOpacity>
+  const computerPlay = () => {
+    let count = 0;
+    let j = 0;
+    let check = Array.from({ length: 3 }, () =>
+      Array.from({ length: 3 }, () => false)
     );
-  }
+    for (let i = 0; i < 3; i++) {
+      j = i;
+      // 연결되는 땅 count가 2인곳을 우선 찾음
+      if (playGround[i][j] == computer && !check[i][j]) {
+        count = getGroundData(i + 1, j) - getGroundData(i - 1, j);
+        if (count === 2) {
+          i = i - 1;
+          return { i, j };
+        } else if (count === -2) {
+          i = i + 1;
+          return { i, j };
+        }
+
+        count = getGroundData(i, j + 1) - getGroundData(i, j - 1);
+        if (count === 2) {
+          j = j - 1;
+          return { i, j };
+        } else if (count === -2) {
+          j = j + 1;
+          return { i, j };
+        }
+
+        count = getGroundData(i + 1, j + 1) - getGroundData(i - 1, j - 1);
+        if (count === 2) {
+          i = i - 1;
+          j = j - 1;
+          return { i, j };
+        } else if (count === -2) {
+          i = i + 1;
+          j = j + 1;
+          return { i, j };
+        }
+
+        count = getGroundData(i - 1, j + 1) - getGroundData(i + 1, j - 1);
+        if (count === 2) {
+          i = i + 1;
+          j = j - 1;
+          return { i, j };
+        } else if (count === -2) {
+          i = i - 1;
+          j = j + 1;
+          return { i, j };
+        }
+      }
+      
+    }
+
+    // 상대의 연결되는 땅 count가 2인곳을 찾음
+  };
+
+  const [titleMessage, setTitleMessage] = useState("Start Game!");
+  const [content, setContent] = useState("");
 
   const changeContent = (key) => {
     console.log(key);
