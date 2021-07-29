d6faa1ae1918fbd43f3a0579072387a25ceb395f
DongheonKim <oueya1479@naver.com>
2021-07-29 20:50:57 +0900

    Add tictactoe gameScreen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,61 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native'; 
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-const GameScreen = props => {};
+const GameScreen = (props) => {
+  const [titleMessage, setTitleMessage] = useState("Start Game!");
 
-const styles = StyleSheet.create({});
+  let squares = [];
+  for (let i = 0; i < 9; i++) {
+    squares.push(<TouchableOpacity key={i} style={styles.buttonContainer} />);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{titleMessage}</Text>
+      <View style={styles.row}>
+        {squares[0]}
+        {squares[1]}
+        {squares[2]}
+      </View>
+      <View style={styles.row}>
+        {squares[3]}
+        {squares[4]}
+        {squares[5]}
+      </View>
+      <View style={styles.row}>
+        {squares[6]}
+        {squares[7]}
+        {squares[8]}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 0,
+    padding: 0,
+  },
+  title: {
+    fontSize: 30,
+    textAlign: "center",
+    marginBottom: 20
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 300,
+    height: 100,
+    overflow: "hidden",
+  },
+  buttonContainer: {
+    borderColor: "black",
+    flex: 1,
+    borderWidth: 5,
+    width: 100,
+    height: 100,
+  },
+});
 
 export default GameScreen;
