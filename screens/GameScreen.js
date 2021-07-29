287d82a48ca79649981c76871036eb6f29ac7305
DongheonKim <oueya1479@naver.com>
2021-07-29 22:24:40 +0900

    test

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,119 +1,37 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-import playerCode from "../constants/player-code";
-
 const GameScreen = (props) => {
   const [titleMessage, setTitleMessage] = useState("Start Game!");
-  const [playGround] = useState(
-    Array.from({ length: 3 }, () => Array.from({ length: 3 }, () => 0))
-  );
+  const [content, setContent] = useState("");
+
+  const changeContent = (key) => {
+    setContent((currentContent) => {
+      if (currentContent === "") setContent("X");
+      else setContent("");
+    });
+  };
 
   let squares = [];
   for (let i = 0; i < 9; i++) {
-    squares.push(<TouchableOpacity key={i} style={styles.buttonContainer} />);
+    squares.push(
+      <TouchableOpacity
+        key={i}
+        style={styles.buttonContainer}
+        onPress={changeContent.bind(this, i)}
+      >
+        <Text style={styles.content}>{content}</Text>
+      </TouchableOpacity>
+    );
   }
 
-  const checkFinish = (playerNum) => {
-    //const [possible, setPossible] = useState(0);
-    let count = 0;
-    for (let i = 0; i < 3; i++) {
-      if (playGround[i][i] === playerNum) {
-        setPossible(possible + 1);
-        break;
-      }
-    }
-    if (count === 0) return 0;
-    //(i,i)좌표에 자신의 땅이 하나라도 없으면 승리는 불가능
-    else if (count === 3) return playerNum;
+  const [playGound] = useState(
+    Array.from({ length: 3 }, () => Array.from({ length: 3 }, () => 0))
+  );
 
-    for (let i = 0; i < 3; i++) {
-      if (
-        playGround[i][0] === playerNum &&
-        playGround[i][1] === playerNum &&
-        playGround[i][2] === playerNum
-      ) {
-        return playerNum;
-      }
-      if (
-        playGround[0][i] === playerNum &&
-        playGround[1][i] === playerNum &&
-        playGround[2][i] === playerNum
-      ) {
-        return playerNum;
-      }
-    }
+  const checkFinish = () => {};
 
-    if (
-      playGound[2][0] === playerNum &&
-      playGound[1][1] === playerNum &&
-      playGound[0][2] === playerNum
-    ) {
-      return playerNum;
-    }
-  };
-
-  const getGroundData = (c, r) => {
-    if (c < 0) c = 3 + c;
-    if (r < 0) r = 3 + r;
-    return playGround[c][r];
-  };
-
-  const computerPlay = () => {
-    let count = 0;
-    let j = 0;
-    let check = Array.from({ length: 3 }, () =>
-      Array.from({ length: 3 }, () => false)
-    );
-    for (let i = 0; i < 3; i++) {
-      j = i;
-      // 연결되는 땅 count가 2인곳을 우선 찾음
-      if (playGround[i][j] == computer && !check[i][j]) {
-        count = getGroundData(i + 1, j) - getGroundData(i - 1, j);
-        if (count === 2) {
-          i = i - 1;
-          return { i, j };
-        } else if (count === -2) {
-          i = i + 1;
-          return { i, j };
-        }
-
-        count = getGroundData(i, j + 1) - getGroundData(i, j - 1);
-        if (count === 2) {
-          j = j - 1;
-          return { i, j };
-        } else if (count === -2) {
-          j = j + 1;
-          return { i, j };
-        }
-
-        count = getGroundData(i + 1, j + 1) - getGroundData(i - 1, j - 1);
-        if (count === 2) {
-          i = i - 1;
-          j = j - 1;
-          return { i, j };
-        } else if (count === -2) {
-          i = i + 1;
-          j = j + 1;
-          return { i, j };
-        }
-
-        count = getGroundData(i - 1, j + 1) - getGroundData(i + 1, j - 1);
-        if (count === 2) {
-          i = i + 1;
-          j = j - 1;
-          return { i, j };
-        } else if (count === -2) {
-          i = i - 1;
-          j = j + 1;
-          return { i, j };
-        }
-      }
-      
-    }
-
-    // 상대의 연결되는 땅 count가 2인곳을 찾음
-  };
+  const computerPlay = () => {};
 
   return (
     <View style={styles.container}>
@@ -154,6 +72,11 @@ const styles = StyleSheet.create({
     width: 300,
     height: 100,
     overflow: "hidden",
+  },
+  content: {
+    fontSize: 80,
+    paddingVertical: 0,
+    textAlign: "center",
   },
   buttonContainer: {
     borderColor: "black",
