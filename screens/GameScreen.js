59c00cfd11aa5da37cc3ed4d092159186d29e727
DongheonKim <oueya1479@naver.com>
2021-07-30 14:28:19 +0900

    Update GameScreen.js

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 import playerCode from "../constants/player-code";
